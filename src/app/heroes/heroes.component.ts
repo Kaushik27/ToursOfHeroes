73e8ee4e75df4e68764b9ba853367b94eae36918
Kaushik Mandal <kaushikmandal27@gmail.com>
2019-07-18 01:31:04 +0530

    http and in memory db

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -23,7 +23,7 @@ export class HeroesComponent implements OnInit {
   // }
 
   constructor(private heroService: HeroService) { }
-  
+
 
   ngOnInit() {
     this.getHeroes();
@@ -47,6 +47,20 @@ export class HeroesComponent implements OnInit {
 
   getHeroes(): void {
     this.heroService.getHeroes()
-        .subscribe(heroes => this.heroes = heroes);
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter(h => h !== hero);
+    this.heroService.deleteHero(hero).subscribe();
   }
 }
